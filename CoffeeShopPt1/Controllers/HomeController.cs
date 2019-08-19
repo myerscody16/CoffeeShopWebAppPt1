33801267c5f122f2558cd9696a4525677724a65c
myerscody16 <myerscody16@live.com>
2019-08-18 20:18:07 -0400

    initial

---
+++ CoffeeShopPt1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoffeeShopPt1.Models;
+
+namespace CoffeeShopPt1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View(new NewUser());
+        }
+        [HttpPost]
+        public IActionResult Registration(NewUser newUser)
+        {
+            return RedirectToAction("EnrollNewUser", newUser);
+        }
+        public IActionResult EnrollNewUser(NewUser user)
+        {
+            return View(user);
+        }
+    }
+}
---
+++ CoffeeShopPt1/Models/RegisterUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopPt1.Models
+{
+    public class NewUser
+    {
+        [StringLength(30, ErrorMessage = "Must be between 3 and 30 characters", MinimumLength = 3)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(30, ErrorMessage = "Must be between 3 and 30 characters", MinimumLength = 3)]
+        [Required]
+        public string LastName { get; set; }
+
+        [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0 - 9a - zA - Z][-\w] *[0 - 9a - zA - Z] *\.)+[a-zA-Z0-9]{2,17})$")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$")]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(30, ErrorMessage = "Must be between 3 and 30 characters", MinimumLength = 3)]
+        [Required]
+        public string Password { get; set; }
+    }
+}
